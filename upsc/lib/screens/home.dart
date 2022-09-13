8aae419d3e412384461ce1cda66d7611733c632e
Dinesh Naik <32676675+Hackerdash@users.noreply.github.com>
2022-09-13 11:15:48 +0530

    .

--- upsc/lib/screens/home.dart
+++ upsc/lib/screens/home.dart
@@ -84,7 +84,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       Text(
                         'Resources',
                         style: TextStyle(fontSize: 20),
-                      )
+                      ),
                     ],
                   ),
                 ),
