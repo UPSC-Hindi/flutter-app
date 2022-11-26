3afa4833d12d8dd00ca9d88f673978fa33647aa1
hackerdash <naik984907@gmail.com>
2022-11-26 20:30:34 +0530

    .

--- upsc/lib/features/data/remote/models/CoursesModel.dart
+++ upsc/lib/features/data/remote/models/CoursesModel.dart
@@ -11,16 +11,16 @@ class CoursesModel {
   final String msg;
 
   factory CoursesModel.fromJson(Map<String, dynamic> json) => CoursesModel(
-    status: json["status"],
+        status: json["status"],
     data: List<CoursesDataModel>.from(json["data"].map((x) => CoursesDataModel.fromJson(x))),
-    msg: json["msg"],
-  );
+        msg: json["msg"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "status": status,
-    "data": List<dynamic>.from(data.map((x) => x.toJson())),
-    "msg": msg,
-  };
+        "status": status,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+        "msg": msg,
+      };
 }
 
 class CoursesDataModel {
@@ -71,52 +71,52 @@ class CoursesDataModel {
   final String createdAt;
 
   factory CoursesDataModel.fromJson(Map<String, dynamic> json) => CoursesDataModel(
-    id: json["_id"],
-    batchName: json["batch_name"],
-    category: Category.fromJson(json["category"]),
-    examType: json["exam_type"],
-    student: List<String>.from(json["student"].map((x) => x)),
+        id: json["_id"],
+        batchName: json["batch_name"],
+        category: Category.fromJson(json["category"]),
+        examType: json["exam_type"],
+        student: List<String>.from(json["student"].map((x) => x)),
     teacher: List<Teacher>.from(json["teacher"].map((x) => Teacher.fromJson(x))),
-    startingDate: DateTime.parse(json["starting_date"]),
-    endingDate: DateTime.parse(json["ending_date"]),
-    mode: json["mode"],
-    materials: json["materials"],
-    language: json["language"],
-    charges: json["charges"],
-    discount: json["discount"],
-    description: json["description"],
+        startingDate: DateTime.parse(json["starting_date"]),
+        endingDate: DateTime.parse(json["ending_date"]),
+        mode: json["mode"],
+        materials: json["materials"],
+        language: json["language"],
+        charges: json["charges"],
+        discount: json["discount"],
+        description: json["description"],
     banner: List<Banner>.from(json["banner"].map((x) => Banner.fromJson(x))),
-    remark: json["remark"],
-    demoVideo: List<dynamic>.from(json["demoVideo"].map((x) => x)),
-    validity: json["validity"],
-    isActive: json["is_active"],
-    courseReview: json["course_review"],
-    createdAt: json["created_at"],
-  );
+        remark: json["remark"],
+        demoVideo: List<dynamic>.from(json["demoVideo"].map((x) => x)),
+        validity: json["validity"],
+        isActive: json["is_active"],
+        courseReview: json["course_review"],
+        createdAt: json["created_at"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "_id": id,
-    "batch_name": batchName,
-    "category": category.toJson(),
-    "exam_type": examType,
-    "student": List<dynamic>.from(student.map((x) => x)),
-    "teacher": List<dynamic>.from(teacher.map((x) => x.toJson())),
+        "_id": id,
+        "batch_name": batchName,
+        "category": category.toJson(),
+        "exam_type": examType,
+        "student": List<dynamic>.from(student.map((x) => x)),
+        "teacher": List<dynamic>.from(teacher.map((x) => x.toJson())),
     "starting_date": "${startingDate.year.toString().padLeft(4, '0')}-${startingDate.month.toString().padLeft(2, '0')}-${startingDate.day.toString().padLeft(2, '0')}",
     "ending_date": "${endingDate.year.toString().padLeft(4, '0')}-${endingDate.month.toString().padLeft(2, '0')}-${endingDate.day.toString().padLeft(2, '0')}",
-    "mode": mode,
-    "materials": materials,
-    "language": language,
-    "charges": charges,
-    "discount": discount,
-    "description": description,
-    "banner": List<dynamic>.from(banner.map((x) => x.toJson())),
-    "remark": remark,
-    "demoVideo": List<dynamic>.from(demoVideo.map((x) => x)),
-    "validity": validity,
-    "is_active": isActive,
-    "course_review": courseReview,
-    "created_at": createdAt,
-  };
+        "mode": mode,
+        "materials": materials,
+        "language": language,
+        "charges": charges,
+        "discount": discount,
+        "description": description,
+        "banner": List<dynamic>.from(banner.map((x) => x.toJson())),
+        "remark": remark,
+        "demoVideo": List<dynamic>.from(demoVideo.map((x) => x)),
+        "validity": validity,
+        "is_active": isActive,
+        "course_review": courseReview,
+        "created_at": createdAt,
+      };
 }
 
 class Banner {
@@ -131,16 +131,16 @@ class Banner {
   final String fileSize;
 
   factory Banner.fromJson(Map<String, dynamic> json) => Banner(
-    fileLoc: json["fileLoc"],
-    fileName: json["fileName"],
-    fileSize: json["fileSize"],
-  );
+        fileLoc: json["fileLoc"],
+        fileName: json["fileName"],
+        fileSize: json["fileSize"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "fileLoc": fileLoc,
-    "fileName": fileName,
-    "fileSize": fileSize,
-  };
+        "fileLoc": fileLoc,
+        "fileName": fileName,
+        "fileSize": fileSize,
+      };
 }
 
 class Category {
@@ -155,16 +155,16 @@ class Category {
   final bool isActive;
 
   factory Category.fromJson(Map<String, dynamic> json) => Category(
-    id: json["_id"],
-    title: json["title"],
-    isActive: json["is_active"],
-  );
+        id: json["_id"],
+        title: json["title"],
+        isActive: json["is_active"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "_id": id,
-    "title": title,
-    "is_active": isActive,
-  };
+        "_id": id,
+        "title": title,
+        "is_active": isActive,
+      };
 }
 
 class Teacher {
@@ -179,14 +179,14 @@ class Teacher {
   final String profilePhoto;
 
   factory Teacher.fromJson(Map<String, dynamic> json) => Teacher(
-    id: json["_id"],
-    fullName: json["FullName"],
-    profilePhoto: json["profilePhoto"],
-  );
+        id: json["_id"],
+        fullName: json["FullName"],
+        profilePhoto: json["profilePhoto"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "_id": id,
-    "FullName": fullName,
-    "profilePhoto": profilePhoto,
-  };
+        "_id": id,
+        "FullName": fullName,
+        "profilePhoto": profilePhoto,
+      };
 }
--- upsc/lib/view/screens/bottomnav/mocktestscreen.dart
+++ upsc/lib/view/screens/bottomnav/mocktestscreen.dart
@@ -69,7 +69,9 @@ class _mocktestscreenState extends State<mocktestscreen> {
                               borderRadius: BorderRadius.circular(20),
                             ),
                           ),
-                          onPressed: () {},
+                             onPressed: () {
+                            Navigator.pushNamed(context, 'mytestseries');
+                          },
                           child: Row(
                             mainAxisSize: MainAxisSize.min,
                             children: const [
