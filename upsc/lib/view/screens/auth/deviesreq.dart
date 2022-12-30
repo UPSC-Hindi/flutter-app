54a01aa448b9a42898560dfc1e3e3238a27a1a7d
“Hackerdash” <naik984907@gmail.com>
2022-12-30 20:51:18 +0530

    toast

---
+++ upsc/lib/view/screens/auth/deviesreq.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DevicesLogin extends StatelessWidget {
+  const DevicesLogin({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DevicesLogin'),
+      ),
+      body: Center(
+        child: Text('DevicesLogin'),
+      ),
+    );
+  }
+}
